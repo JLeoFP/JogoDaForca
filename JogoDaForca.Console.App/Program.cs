883cb40679b4960ec01c48370da7ad864562b438
Leo Fzp <Fernandezpatrijuanl93@gmail.com>
2026-03-17 15:34:42 -0300

    first commit

---
+++ JogoDaForca.Console.App/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Bem-vindo ao Jogo da Forca!");
+        Console.WriteLine("---------------------------------");
+
+
+     
+    }
+
+        static string palavraAleatoria()
+        {
+            string[] palavras = [
+            "ABACATE",
+            "ABACAXI",
+            "ACEROLA",
+            "AÇAÍ",
+            "ARAÇÁ",
+            "ABACATE",
+            "BACABA",
+            "BACURI",
+            "BANANA",
+            "CAJÁ",
+            "CAJU",
+            "CARAMBOLA",
+            "CUPUAÇU",
+            "GRAVIOLA",
+            "GOIABA",
+            "JABUTICABA",
+            "JENIPAPO",
+            "MAÇÃ",
+            "MANGABA",
+            "MANGA",
+            "MARACUJÁ",
+            "MURICI",
+            "PEQUI",
+            "PITANGA",
+            "PITAYA",
+            "SAPOTI",
+            "TANGERINA",
+            "UMBU",
+            "UVA",
+            "UVAIA"
+            ];
+            Random random = new Random();
+            int index = random.Next(palavras.Length);
+            return palavras[index];
+        }
+
+}
